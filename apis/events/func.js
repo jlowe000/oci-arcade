c07ed2f00f4f739c2b3a6d4f11ec4784169daf0b
Jason Lowe <jlowe000@gmail.com>
2020-09-29 08:50:44 +0000

    Squashed commit of the following:
    commit fa9a631ebf91e290b50ad4db21edd7db905320e0
    Author: Jason Lowe <jlowe000@gmail.com>
    Date:   Tue Sep 29 08:40:45 2020 +0000
    
        added event API

--- apis/events/func.js
+++ apis/events/func.js
@@ -2,11 +2,11 @@ const fdk=require('@fnproject/fdk');
 const axios=require('axios');
 
 async function addEvent(input) {
-    adwres = await axios.post('https://@APEX_HOSTNAME@/ords/@APEX_SCHEMA@/event_table/', input, { auth: { username: '@APEX_USER@', password: '@APEX_PASSWORD@' }})
+    adwres = await axios.post('https://@APEX_HOSTNAME@/ords/@APEX_SCHEMA@/event_table/', input, { auth: { username: '@APEX_USER@', password: '@APEX_PASSWORD@' }});
     return adwres.data;
 }
 
-fdk.handle(function(input){
+fdk.handle(function(input,ctx){
   try {
     return addEvent(input);
   } catch (error) {
