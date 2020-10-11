66b444794443a2ba450194312fb1e6fbaed25d2a
Jason Lowe <jlowe000@gmail.com>
2020-10-11 21:40:39 +0000

    used ENV variables for different parameters

--- apis/events/func.js
+++ apis/events/func.js
@@ -1,14 +1,19 @@
 const fdk=require('@fnproject/fdk');
 const axios=require('axios');
 
-async function addEvent(input) {
-    adwres = await axios.post('https://@APEX_HOSTNAME@/ords/@APEX_SCHEMA@/event_table/', input, { auth: { username: '@APEX_USER@', password: '@APEX_PASSWORD@' }});
+async function addEvent(input,ctx) {
+    hostname = ctx.config['ORDS_HOSTNAME'];
+    workspace = ctx.config['APEX_WORKSPACE'];
+    username = ctx.config['API_USER'];
+    password = ctx.config['API_PASSWORD'];
+    adwres = await axios.post('https://'+hostname+'/ords/'+workspace+'/event_table/', input, { auth: { username: username, password: password }});
     return adwres.data;
 }
 
+
 fdk.handle(function(input,ctx){
   try {
-    return addEvent(input);
+    return addEvent(input,ctx);
   } catch (error) {
     console.log(error);
     return { 'response' : 'bad' };
