bd9eac03b30fb9d88f2c34661b5337a7989412b7
Jason Lowe <jlowe000@gmail.com>
2021-01-28 18:40:11 +1000

    initial update (to be tested) using OAuth token

--- apis/events/serverless/func.js
+++ apis/events/serverless/func.js
@@ -1,15 +1,42 @@
 const fdk=require('@fnproject/fdk');
 const axios=require('axios');
 
-async function addEvent(input,ctx) {
-    hostname = ctx.config['ORDS_HOSTNAME'];
-    workspace = ctx.config['APEX_WORKSPACE'];
-    username = ctx.config['API_USER'];
-    password = ctx.config['API_PASSWORD'];
-    adwres = await axios.post('https://'+hostname+'/ords/'+workspace+'/event_table/', input, { auth: { username: username, password: password }});
-    return adwres.data;
+let access_token = '';
+
+let ORDS_HOSTNAME = '';
+let APEX_WORKSPACE = '';
+let API_USER = '';
+let API_PASSWORD = '';
+
+const token_refresh_interval = 1800000;
+
+function getToken() {
+    console.log('retrieve token');
+    if (ORDS_HOSTNAME != '') {
+        axios.post('https://'+ORDS_HOSTNAME+'/ords/'+APEX_WORKSPACE+'/oauth/token', 'grant_type=client_credentials', { auth: { username: API_USER, password: API_PASSWORD }})
+        .then(oauthres => {
+                  access_token = oauthres.data.access_token;
+	          console.log('token:'+access_token);
+              })
+        .catch(err => {
+                  console.log(err);
+                  exit(-1);
+              });
+    }
 }
 
+getToken();
+setInterval(getToken, token_refresh_interval);
+
+async function addEvent(input,ctx) {
+    ORDS_HOSTNAME = ctx.config['ORDS_HOSTNAME'];
+    APEX_WORKSPACE= ctx.config['APEX_WORKSPACE'];
+    API_USER = ctx.config['API_USER'];
+    API_PASSWORD= ctx.config['API_PASSWORD'];
+    getToken();
+    adwres = await axios.post('https://'+ORDS_HOSTNAME+'/ords/'+APEX_WORKSPACE+'/event_table/', input, { headers: { 'Authorization': 'Bearer '+access_token }});
+    return adwres.data;
+}
 
 fdk.handle(function(input,ctx){
   try {
