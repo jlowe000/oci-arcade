74a024f02fe6aa6bd6551a53c3a2ffa1881c84dc
Jason Lowe <jlowe000@gmail.com>
2021-01-28 15:56:31 +1000

    removed the zip file of the ORM and replaced with a directory of the same content the ORM allows for a ORM to be passed in as a directory.

---
+++ infra/tenancy/tf-ociarcade/vars.tf
@@ -0,0 +1,5 @@
+variable arcade-web_source_image_id { default = "ocid1.image.oc1.ap-sydney-1.aaaaaaaasbprumtin47wvqprgbzbn6wrzct7afonp72lwur6bisgjq6rqphq" }
+variable region { default = "ap-sydney-1" }
+variable compartment_ocid { default = "ocid1.compartment.oc1..XXXX" }
+variable custom_ssh_key { default = "ssh-rsa XXXX" }
+variable custom_adb_admin_password { default = "Welcome123!#" }
