b5a7bcec18f7e95150dca80aed25224a1495104d
Jason Lowe <jlowe000@gmail.com>
2021-01-28 15:20:49 +1000

    fixed the relative directory of the openssl keys for the self-signed certs

--- bin/api-score-docker-build.sh
+++ bin/api-score-docker-build.sh
@@ -1,5 +1,5 @@
 if [ ! -f infra/compute/key.pem ]; then
-    openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
+    openssl req -x509 -newkey rsa:4096 -keyout infra/compute/key.pem -out infra/copute/cert.pem -days 365
 fi
 docker images
 docker rmi api-score
