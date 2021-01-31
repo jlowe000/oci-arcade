905009eb213a50849fbbec6283c61007dd8f0cd2
Jason Lowe <jlowe000@gmail.com>
2021-01-31 19:39:21 +1000

    fixed cert creation

--- bin/api-score-docker-build.sh
+++ bin/api-score-docker-build.sh
@@ -13,8 +13,8 @@ if [ ! -f infra/compute/key.pem ]; then
     OPENSSL_PWD=""
     if [ "$2" != "" ]
 	then 
-	  OPENSSL_PWD="pass:$2"
+	  OPENSSL_PWD="-passin pass:$2"
 	fi
-    openssl req -x509 -newkey rsa:4096 ${OPENSSL_PWD} -keyout infra/compute/key.pem -out infra/copute/cert.pem -days 365
+    openssl req -x509 -newkey rsa:4096 ${OPENSSL_PWD} -subj /CN=AU/ -keyout infra/compute/key.pem -out infra/compute/cert.pem -days 365
 fi
 docker build ${BUILD_OPTIONS} --tag api-score --file containers/web/api-score.Dockerfile .
