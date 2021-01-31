cfe4b65b522e2d757984420a8774baa4565da4a1
Jason Lowe <jlowe000@gmail.com>
2021-01-31 19:57:02 +1000

    indenting issues

--- bin/api-score-docker-build.sh
+++ bin/api-score-docker-build.sh
@@ -10,11 +10,11 @@ then
   BUILD_OPTIONS="${BUILD_OPTIONS} --build-arg API_PASSWORD=$2"
 fi 
 if [ ! -f infra/compute/key.pem ]; then
-    OPENSSL_PWD=""
-    if [ "$2" != "" ]
-	then 
-	  OPENSSL_PWD="-passin pass:$2"
-	fi
-    openssl req -x509 -newkey rsa:4096 ${OPENSSL_PWD} -subj /CN=AU/ -keyout infra/compute/key.pem -out infra/compute/cert.pem -days 365
+  OPENSSL_PWD=""
+  if [ "$2" != "" ]
+  then 
+    OPENSSL_PWD="-passin pass:$2"
+  fi
+  openssl req -x509 -newkey rsa:4096 ${OPENSSL_PWD} -subj /CN=AU/ -keyout infra/compute/key.pem -out infra/compute/cert.pem -days 365
 fi
 docker build ${BUILD_OPTIONS} --tag api-score --file containers/web/api-score.Dockerfile .
