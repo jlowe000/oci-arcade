e48b2cb32d0cd806dae23e8fc754e3da0bb56711
Jason Lowe <jlowe000@gmail.com>
2021-01-31 18:59:48 +1000

    added in logic to add a passphrase through arguments - not secure

--- bin/api-score-docker-build.sh
+++ bin/api-score-docker-build.sh
@@ -1,8 +1,6 @@
-if [ ! -f infra/compute/key.pem ]; then
-    openssl req -x509 -newkey rsa:4096 -keyout infra/compute/key.pem -out infra/copute/cert.pem -days 365
-fi
 docker images
 docker rmi api-score
+BUILD_OPTIONS=""
 if [ "$1" != "" ]
 then
   BUILD_OPTIONS="${BUILD_OPTIONS} --build-arg ORDS_HOSTNAME=$1"
@@ -11,4 +9,11 @@ if [ "$2" != "" ]
 then
   BUILD_OPTIONS="${BUILD_OPTIONS} --build-arg API_PASSWORD=$2"
 fi 
+if [ ! -f infra/compute/key.pem ]; then
+    if [ "$2" != "" ]
+	then 
+	  OPENSSL_PWD="pass:$2"
+	fi
+    openssl req -x509 -newkey rsa:4096 ${OPENSSL_PWD} -keyout infra/compute/key.pem -out infra/copute/cert.pem -days 365
+fi
 docker build ${BUILD_OPTIONS} --tag api-score --file containers/web/api-score.Dockerfile .
