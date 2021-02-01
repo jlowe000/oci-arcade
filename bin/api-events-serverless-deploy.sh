b48f9d1683b2be7251e0a7276e8d55e5e34c6bb9
Jason Lowe <jlowe000@gmail.com>
2021-02-01 15:32:46 +1000

    adding in parameterised func for events data

--- bin/api-events-serverless-deploy.sh
+++ bin/api-events-serverless-deploy.sh
@@ -1,3 +1,9 @@
-cd apis/events/serverless
+cd apis/events/serverless/python
+cp func.yaml.template func.yaml
+echo "config:" >> func.yaml
+echo "  ORDS_HOSTNAME: $1" >> func.yaml
+echo "  APEX_WORKSPACE: $2" >> func.yaml
+echo "  API_USER: $3" >> func.yaml
+echo "  API_PASSWORD: $4" >> func.yaml
 fn deploy --app events --local
 cd -
