7e2d8a794d3545ab165a792b3d2915d400113cc8
Jason Lowe <jlowe000@gmail.com>
2021-02-01 20:06:34 +1000

    added additional URL and templates for simplicity

--- infra/tenancy/tf-ociarcade/outputs.tf
+++ infra/tenancy/tf-ociarcade/outputs.tf
@@ -14,3 +14,18 @@ output "region" {
   value = var.region
 }
 
+output "pacman_url" {
+  value = "https://objectstorage.${var.region}.oraclecloud.com/n/${var.bucket_ns}/b/oci-arcade/o/pacman/index.htm"
+}
+
+output "spaceinvaders_url" {
+  value = "https://objectstorage.${var.region}.oraclecloud.com/n/${var.bucket_ns}/b/oci-arcade/o/spaceinvaders/index.htm"
+}
+
+output "sqldev_url" {
+  value = replace(lookup(oci_database_autonomous_database.export_arcade.connection_urls[0],"sql_dev_web_url"),"admin","ociarcade")
+}
+
+output "ssh template" {
+  value = "ssh -i ~/.ssh/id_rsa opc@${oci_core_instance.export_arcade-web.public_ip}"
+}
