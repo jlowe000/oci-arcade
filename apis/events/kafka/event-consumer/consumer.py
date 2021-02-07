fab2f6c8438d415ca6d4d708f2349cd803b0e78f
Jason Lowe <jlowe000@gmail.com>
2021-02-08 06:52:20 +1000

    added scripts to augment kafka parameters into project

--- apis/events/kafka/event-consumer/consumer.py
+++ apis/events/kafka/event-consumer/consumer.py
@@ -10,6 +10,8 @@ ORDS_HOSTNAME = os.environ.get('ORDS_HOSTNAME')
 APEX_WORKSPACE = os.environ.get('APEX_WORKSPACE')
 API_USER = os.environ.get('API_USER')
 API_PASSWORD = os.environ.get('API_PASSWORD')
+BOOTSTRAP_SERVER = os.environ.get('BOOTSTRAP_SERVER')
+TOPIC = os.environ.get('TOPIC')
 
 client = KafkaClient(hosts='kafka_kafka_1:9092')
 print(client.topics)
@@ -21,7 +23,9 @@ for msg in consumer:
     try:
       print(msg.value)
       payload = json.loads(msg.value)
-      res = requests.post('https://'+ORDS_HOSTNAME+'/ords/'+APEX_WORKSPACE+'/event_table/', payload, auth = HTTPBasicAuth(API_USER, API_PASSWORD));
+      res = requests.post('https://'+ORDS_HOSTNAME+'/ords/'+APEX_WORKSPACE+'/oauth/token', data={'grant_type':'client_credentials'}, auth=HTTPBasicAuth(API_USER, API_PASSWORD));
+      access_token = res.json()['access_token'];
+      res = requests.post('https://'+ORDS_HOSTNAME+'/ords/'+APEX_WORKSPACE+'/event_table/', payload, headers={ 'Authorization': 'Bearer '+access_token});
       print(res)
     except:
       print('err:',sys.exc_info()[0]);
--- bin/oci-kafka-event-build.sh
+++ bin/oci-kafka-event-build.sh
@@ -1,3 +1,16 @@
 docker images
 docker rmi oci-kafka-events
-docker build --tag oci-kafka-events --file containers/kafka/oci-kafka-events.Dockerfile .
+BUILD_OPTIONS=""
+if [ "$1" != "" ]; then
+  BUILD_OPTIONS="${BUILD_OPTIONS} --build-arg ORDS_HOSTNAME=$1"
+  BUILD_OPTIONS="${BUILD_OPTIONS} --build-arg BOOTSTRAP_SERVER=$2"
+  BUILD_OPTIONS="${BUILD_OPTIONS} --build-arg TOPIC=$3"
+fi 
+if [ -f infra/db/client.csv ]; then
+  LINE=`cat infra/db/client.csv`
+  API_USER=`echo $LINE | cut -f 1 -d ,`
+  API_PASSWORD=`echo $LINE | cut -f 2 -d ,`
+  BUILD_OPTIONS="${BUILD_OPTIONS} --build-arg API_USER=$API_USER"
+  BUILD_OPTIONS="${BUILD_OPTIONS} --build-arg API_PASSWORD=$API_PASSWORD"
+fi
+docker build --tag oci-kafka-events ${BUILD_OPTIONS} --file containers/kafka/oci-kafka-events.Dockerfile .
