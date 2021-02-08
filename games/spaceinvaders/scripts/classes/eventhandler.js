3739b3c405639c774c933fcb7df44a905f27729a
Jason Lowe <jlowe000@gmail.com>
2021-02-09 06:25:24 +1000

    changed to include the event channel type through the index.htm

--- games/spaceinvaders/scripts/classes/eventhandler.js
+++ games/spaceinvaders/scripts/classes/eventhandler.js
@@ -1,12 +1,15 @@
-const EVENT_BASE_URL = 'https://'+API_HOSTNAME+':8081/event/serverless';
+window.pubType = sessionStorage.getItem("pubtype");
+var EVENT_BASE_URL = 'https://'+API_HOSTNAME+':8081/event/'+window.pubType;
 
 var EventHandler = (function() {
     return {
         addEvent: async function (input) {
             try {
-                let response = await axios.post(EVENT_BASE_URL,input);
-                console.log('event-response:'+response)
-                return response;
+		if (window.pubType !== "none") {
+                    let response = await axios.post(EVENT_BASE_URL,input);
+                    console.log('event-response:'+response)
+                    return response;
+                }
             } catch (err) {
                 console.log('event-error:'+err)
             }
