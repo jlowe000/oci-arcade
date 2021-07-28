d288ace0c94ec85cb332d31bbe4b9d4d35c6f465
Jason Lowe <jlowe000@gmail.com>
2021-07-28 13:42:32 +1000

    added CRM scripts

---
+++ apis/user/db/init-crm-app.sql
@@ -0,0 +1,6 @@
+declare
+    id number;
+begin
+    apex_util.set_workspace ('OCIARCADE');
+    id := APEX_PKG_APP_INSTALL.INSTALL('Customer Tracker',APEX_AUTHENTICATION.C_TYPE_APEX_ACCOUNTS,'OCIARCADE');
+end;
