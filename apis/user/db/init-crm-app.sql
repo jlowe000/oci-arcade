6013265e9f117850d466a8e200e2aa115d5d93d0
Jason Lowe <jlowe000@gmail.com>
2022-04-10 09:54:30 +1000

    updated to install customer tracker from oracle apex git repo

--- apis/user/db/init-crm-app.sql
+++ apis/user/db/init-crm-app.sql
@@ -1,8 +1,19 @@
 declare
-    id number;
+    l_source apex_t_export_files;
 begin
-    apex_util.set_workspace ('OCIARCADE');
-    id := APEX_PKG_APP_INSTALL.INSTALL('Customer Tracker',APEX_AUTHENTICATION.C_TYPE_APEX_ACCOUNTS,'OCIARCADE');
+    l_source := apex_t_export_files (
+        apex_t_export_file (
+            name     => 'customers.sql',
+            contents => apex_web_service.make_rest_request (
+                p_url         => 'https://github.com/oracle/apex/raw/21.2/starter-apps/customers/customers.sql',
+                p_http_method => 'GET' )));
+
+    apex_util.set_workspace('OCIARCADE');
+    apex_application_install.generate_application_id;
+    apex_application_install.generate_offset;
+    apex_application_install.set_authentication_scheme ( p_name => null );
+    apex_application_install.set_auto_install_sup_obj( p_auto_install_sup_obj => true );
+    apex_application_install.install(p_source => l_source );
 end;
 /
 commit;
