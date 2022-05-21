a8d664cbbcf8dbe57d04bcf93ba8ed13beb7004c
Jason Lowe <jlowe000@gmail.com>
2022-05-21 11:25:15 +1000

    updated the game to new sprites relating to cloud consumption.

--- bin/oci-arcade-storage-build.sh
+++ bin/oci-arcade-storage-build.sh
@@ -1,6 +1,6 @@
 echo "const API_HOSTNAME = '$1';" > games/pacman/api_hostname.js
 echo "const API_HOSTNAME = '$1';" > games/spaceinvaders/scripts/api_hostname.js
 cd games
-python3 ../bin/bulk-upload.py pacman
-python3 ../bin/bulk-upload.py spaceinvaders
+python3 ../bin/bulk-upload.py consume-cloud
+# python3 ../bin/bulk-upload.py spaceinvaders
 cd -
