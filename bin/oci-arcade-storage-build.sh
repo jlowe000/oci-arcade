de15f8e31ae1c97ddedab90d0e054b09dabe3564
jlowe000 <jlowe000@gmail.com>
2022-08-12 09:40:03 +1000

    Update oci-arcade-storage-build.sh

--- bin/oci-arcade-storage-build.sh
+++ bin/oci-arcade-storage-build.sh
@@ -1,4 +1,4 @@
-echo "const API_HOSTNAME = '$1';" > games/pacman/api_hostname.js
+echo "const API_HOSTNAME = '$1';" > games/consume-cloud/api_hostname.js
 echo "const API_HOSTNAME = '$1';" > games/spaceinvaders/scripts/api_hostname.js
 cd games
 python3 ../bin/bulk-upload.py consume-cloud
